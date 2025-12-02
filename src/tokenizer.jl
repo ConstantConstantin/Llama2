1c0955357368ecfb1b95717785a227d4f17a856b
ConstantConstantin <paul.hertel@aol.de>
2025-12-03 00:37:43 +0100

    Fix type problems

--- src/tokenizer.jl
+++ src/tokenizer.jl
@@ -14,43 +14,43 @@ function compare_tokens(first_token::TokenIndex, second_token::TokenIndex)::Bool
 end
 
 struct Tokenizer{
-    VVI<:AbstractVector{AbstractVector{UInt8}}, 
+    VVI<:AbstractVector{<:AbstractVector{UInt8}}, 
     VF<:AbstractVector{Float32},  
     VI<:AbstractVector{UInt8}, 
     I<:Integer,
     J<:Integer,
-    VTI<:AbstractVector{TokenIndex{VI,I}}
+    VTI<:AbstractVector{<:TokenIndex}
 }
     vocab::VVI
     vocab_scores::VF
-    sorted_vocab::VTI
+    byte_pieces::VI
     vocab_size::I
     max_token_length::J
-    byte_pieces::VI
+    sorted_vocab::VTI
 
     function Tokenizer(vocab::VVI, 
         vocab_scores::VF, 
-        sorted_vocab::VTI, 
+        byte_pieces::VI,
         vocab_size::I, 
-        max_token_length::J, 
-        byte_pieces::VI
+        max_token_length::J,
+        sorted_vocab::VTI
     ) where {
-        VVI<:AbstractVector{AbstractVector{UInt8}}, 
+        VVI<:AbstractVector{<:AbstractVector{UInt8}}, 
         VF<:AbstractVector{Float32}, 
         VI<:AbstractVector{UInt8}, 
         I<:Integer,
         J<:Integer,
-        VTI<:AbstractVector{TokenIndex{VI,I}}
+        VTI<:AbstractVector{<:TokenIndex}
     }
         max_token_length < 0 && error("max_token_length must be > 0.")
         length(byte_pieces) != 256 && error("Length of byte_pieces must be 256.")
         
-        new{VVI, VF, VTI, VI, I, J}(vocab, vocab_scores, sorted_vocab, vocab_size, max_token_length, byte_pieces)
+        new{VVI, VF, VI, I, J, VTI}(vocab, vocab_scores, byte_pieces, vocab_size, max_token_length, sorted_vocab)
     end
 end
 function Tokenizer(tokenizer_path::String, vocab_size::Integer)
     byte_pieces = collect(UInt8.(0:255))
-    sorted_vocab = Vector{TokenIndex}()
+    sorted_vocab = Vector{TokenIndex{Vector{UInt8}, Int}}()
 
     vocab_scores = Vector{Float32}(undef, vocab_size)
     vocab = Vector{Vector{UInt8}}(undef, vocab_size)
@@ -66,5 +66,5 @@ function Tokenizer(tokenizer_path::String, vocab_size::Integer)
         end
     end
 
-    Tokenizer(vocab, vocab_scores, sorted_vocab, vocab_size, max_token_length, byte_pieces)
+    Tokenizer(vocab, vocab_scores, byte_pieces, vocab_size, max_token_length, sorted_vocab)
 end
